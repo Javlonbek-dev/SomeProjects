cede890843bed04f521ca233302bafe1079a18f1
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2022-10-18 09:28:21 +0300

    Snake Game Project

---
+++ source/repos/SnakeOver/SnakeOver/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace SnakeOver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblFood = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(12, 24);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(110, 25);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "Score:0";
+            // 
+            // lblFood
+            // 
+            this.lblFood.BackColor = System.Drawing.Color.Red;
+            this.lblFood.Location = new System.Drawing.Point(666, 262);
+            this.lblFood.Name = "lblFood";
+            this.lblFood.Size = new System.Drawing.Size(21, 21);
+            this.lblFood.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateBlue;
+            this.ClientSize = new System.Drawing.Size(1000, 500);
+            this.Controls.Add(this.lblFood);
+            this.Controls.Add(this.lblScore);
+            this.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblFood;
+    }
+}
+
---
+++ source/repos/SnakeOver/SnakeOver/Piece.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SnakeOver
+{
+     class Piece:Label
+    {
+        public Piece(int x,int y)
+        {
+            Location =new System.Drawing.Point(x,y);
+            Size=new System.Drawing.Size(20,20);
+            BackColor = System.Drawing.Color.Orange;
+            Enabled = false;
+        }
+
+    }
+}
