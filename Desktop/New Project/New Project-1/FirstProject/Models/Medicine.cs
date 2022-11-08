33e50439e93d27ec3bae0a63d23a4228be20a4d0
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2022-11-08 06:15:16 +0300

    Code Rope:

---
+++ Desktop/New Project/New Project-1/FirstProject/Models/Medicine.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using System;
+
+namespace FirstProject.Models
+{
+    public class Medicine
+    {
+        public string Name { get; set; }
+        public string Form  { get; set; }
+        public int Doza  { get; set; }
+        public DataSetDateTime dataTimeOffSet  { get; set; }
+        public int YaroqlilikMuddati { get; set; }
+        public string IshlabChiqruvchi { get; set; }
+
+    }
+}
